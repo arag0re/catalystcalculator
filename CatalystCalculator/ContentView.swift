84e0dc36b1dae4a6410176276d307009165f58aa
Anton Stadie <as@4fo.de>
2019-08-21 15:05:28 +0200

    Initial Commit

---
+++ CatalystCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatalystCalculator
+//
+//  Created by Anton Stadie on 21.08.19.
+//  Copyright © 2019 Anton Stadie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
